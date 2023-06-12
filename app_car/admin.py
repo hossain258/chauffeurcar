02da7b5f2a0267463e4124d34b95a649d1bec575
MOHAMMAD HOSSAIN <hossain258@gmail.com>
2023-06-12 16:50:45 +0100

    form complete

--- app_car/admin.py
+++ app_car/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-from.models import Contact
+from.models import Contact,Services
 
 # Register your models here.
+
+
+@admin.register(Services)
+class serviceAdmin(admin.ModelAdmin):
+    list_display=('title','headline','service_img','created_at')
+    
+
 
 
 
 @admin.register(Contact)
 class contactAdmin(admin.ModelAdmin):
-    list_display=('name','email','phone_number','date_created')
+    list_display=('name','email','phone_number','created_at')
     search_fields = ('name',)
-    list_filter =('date_created',)
+    list_filter =('created_at',)
     list_per_page = 10
--- app_car/models.py
+++ app_car/models.py
@@ -3,13 +3,33 @@ from django.db import models
 # Create your models here.
 
 
-class Contact(models.Model):
-    name = models.CharField(max_length=25)
-    email = models.EmailField(max_length=155)
-    phone_number = models.CharField(max_length=20, null=True, blank=True)
-    message = models.TextField(max_length=1200)
-    date_created = models.DateTimeField(auto_now_add=True)
+class Basemodel(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True, )
+    class Meta:
+        abstract = True
+        ordering = ("-created_at",)
 
+
+class Contact(Basemodel):
+    name = models.CharField(max_length=25, blank=True, null=True)
+    email = models.EmailField(max_length=155, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    message = models.TextField(max_length=1200, blank=True, null=True)
+    
     def __str__(self):
         return self.name
+    
 
+
+class Services(Basemodel):
+    title=models.CharField(max_length=120, blank=True, null=True)
+    headline=models.CharField(max_length=500, blank=True, null=True)
+    service_img=models.ImageField(upload_to='services', blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Courses"
+
+
+
+    def __str__(self):
+        return self.title
